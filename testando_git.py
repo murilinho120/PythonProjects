f776852877dd4f8da92b6270fe593fe46c0bb7b1
Murilo Henrique <murilohenrisaraiva@gmail.com>
2024-07-19 01:03:37 -0300

    testes git hubb

---
+++ testando_git.py
@@ -0,0 +1,16 @@
+#enviar commit para git
+
+# git add .
+
+# git commit -m "Sua mensagem de commit"
+
+# git push
+
+
+
+print("hello word ksksksksk")
+#pegar do remoto e colocoar no local
+
+#git pull    (usar para não ter conflitos) e dar git pull antes de alterar algu,m code
+
+a = input("oioioi")
